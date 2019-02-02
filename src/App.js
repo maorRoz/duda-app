e14a65b3515df501c1e64afafe7d0a4c3f9ad60b
maorroz <andory@post.bgu.ac.il>
2019-02-02 12:29:02 +0200

    added the initial of the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Button>hello</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
